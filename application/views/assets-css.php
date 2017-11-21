637e017fc5881b2e169a05320c60155119a93fad
rizkiramadhan2 <rizki2500@gmail.com>
2017-11-21 14:53:10 +0700

    update metatag

--- application/views/assets-css.php
+++ application/views/assets-css.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="Konveksi Malang, Harga terjangkau siap kirim ke seluruh Indonesia">
+  <meta name="description" content="Konveksi Malang - Harga terjangkau siap kirim ke seluruh Indonesia">
   <meta name="keywords" content="Konveksi Malang, Konveksi di Malang, Konveksi Murah, Konveksi, Pembuatan Baju Malang, Pembuatan Jaket Malang, Pembuatan Jaket Murah">
   <link rel="stylesheet" href="<?php echo base_url().'css/bootstrap.min.css'?>">
   <link rel="stylesheet" type="text/css" href="<?php echo base_url().'css/style.css'?>">
